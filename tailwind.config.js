587d21e526851bb2b0d73a02cf3ccd6aea12f2d1
Ashutosh <iashutoshmishra8@gmail.com>
2021-09-27 16:49:07 +0530

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive'],
+        'roboto': ["Roboto Slab", "cursive"],
+        'montserrat': ["Montserrat", "cursive"]
+      }
+    },
   },
   variants: {
     extend: {},
