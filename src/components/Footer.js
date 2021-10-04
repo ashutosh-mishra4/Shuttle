a4ea0ee3ad02b6c77b5951f62087bcefcd0b6514
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2021-10-04 19:55:15 +0530

    Twitter handle changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="bg-blue-900 text-center py-2 mt-20 text-white bottom-0 w-full">
-          <p>Made with ❤️ by <a href="https://www.twitter.com/clearprogrammer">Ashutosh Mishra</a></p>
+          <p>Made with ❤️ by <a href="https://twitter.com/ashutoshmishrae">Ashutosh Mishra</a></p>
         </div>
     )
 }
