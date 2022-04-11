d3fabe9058e7bb1b62cafc596816b83cd668ea36
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-04-11 22:07:45 +0530

    components imported and react router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { Suspense } from 'react';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-const Hero = React.lazy(() => import('./components/Hero.js'));
-const Sidebar = React.lazy( () => import('./components/Sidebar.js'))
-const News = React.lazy( () => import('./components/News.js'))
-const Footer = React.lazy( () => import('./components/Footer.js'))
+const Hero = React.lazy(() => import("./components/Hero.js"));
+const News = React.lazy(() => import("./components/News.js"));
+const Footer = React.lazy(() => import("./components/Footer.js"));
+const Blogpost = React.lazy(() => import("./components/Blogpost.js"));
+const Reports = React.lazy(() => import("./components/Reports.js"));
+// const Sidebar = React.lazy(() => import('./components/Sidebar.js'))
 
 function App() {
   return (
     <div>
-      <Suspense fallback={
-        <div className=" flex justify-center items-center mt-8">
-          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div>
-        </div>  
-      }>
-        <div className="lg:flex">  
-          <Hero />
-          <Sidebar />
-        </div>
-        <News />
+      <Suspense
+        fallback={
+          <div className=" flex justify-center items-center mt-8">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div>
+          </div>
+        }
+      >
+        <Hero />
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              <Route path="news" element={<News />} />
+              <Route path="blog" element={<Blogpost />} />
+              <Route path="reports" element={<Reports />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
         <Footer />
       </Suspense>
     </div>
