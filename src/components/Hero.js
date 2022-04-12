55b1da77882262511a53f288967b4d836d2c1bb0
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-04-12 21:51:33 +0530

    changed dimensions

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import image from '../assets/Shuttle.png'
 function Hero() {
     return (
         <div>
-            <img src={image} className="max-w-full h-auto" />
+            <img src={image} className="max-w-full h-auto lg:w-screen lg:h-96 lg:object-cover" />
         </div>
     )
 }
