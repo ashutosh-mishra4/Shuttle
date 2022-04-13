3ca5d08f62362cd99a27f72b6bb6b150b7cd595d
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-04-13 22:34:55 +0530

    mobile nav created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiMenuAltLeft } from "react-icons/bi";
-import { GrClose } from 'react-icons/gr'
+import { GrClose } from "react-icons/gr";
 
 function Header() {
+  const [isHidden, setIsHIdden] = useState(true);
+  const handleHidden = () => {
+    setIsHIdden(!isHidden);
+  };
   return (
-    <header className="flex items-center gap-2 text-xl font-montserrat font-bold p-2 ">
-      {/* <nav>
-            <BiMenuAltLeft />
-        </nav> */}
-      <div className="w-40 h-auto border-2 border-black p-4">
-          <GrClose className="text-white bg-white float-right"/>
+    <header className="flex items-center gap-2 text-xl font-montserrat border-2 drop-shadow-md border-black w-screen bg-white font-bold p-2 fixed">
+      {isHidden ? (
+        <BiMenuAltLeft onClick={handleHidden} className="md:hidden" />
+      ) : (
+        <nav className={`w-40 h-auto border-2 border-black p-4`}>
+          <GrClose
+            className="text-white bg-white float-right"
+            onClick={handleHidden}
+          />
           <ul className="text-black flex flex-col justify-center items-center">
-              <li>Home</li>
-              <li>Blog</li>
-              <li>News</li>
-              <li>Reports</li>
+            <li>Home</li>
+            <li>Blog</li>
+            <li>News</li>
+            <li>Reports</li>
           </ul>
-      </div>
+        </nav>
+      )}
+      Shuttle
     </header>
   );
 }
