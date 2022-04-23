d512750ec118144756b6da881119e5daef0c6073
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-04-23 17:38:20 +0530

    added image alt

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import image from '../assets/Shuttle.png'
 function Hero() {
     return (
         <div>
-            <img src={image} className="max-w-full h-auto lg:w-screen lg:h-96 lg:object-cover" />
+            <img src={image} alt="Shuttle Hero with tagline" className="max-w-full h-auto lg:w-screen lg:h-96 lg:object-cover" />
         </div>
     )
 }
